bff4fe3e613e36f7bae512a8f8fddbda44559bc0
Marius <mariussanz24@gmail.com>
2024-10-07 08:59:32 +0200

    despues de problema 2

--- codi.sh
+++ codi.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
-cut -d, -f 12 --complement in.csv
-cut -d, -f 16 --complement in.csv
+cut -d ',' -f 12 --complement in supervivents1.csv
+cut -d ',' -f 16 --complement in supervivents1.csv
 
+awk -F, '{if ($15 == "True") cut}'
+
+awk -F
