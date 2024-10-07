76b6554d30e49bc2ede07ab804df768eb9cc2891
Adrià <adriamoreno@col-legivirolai.net>
2024-10-07 10:27:40 +0200

    2 ejercico

--- codigo.sh
+++ codigo.sh
@@ -1,3 +1,7 @@
-!/bin/bash
-cut -d ',' -f 1,2,3,4,5,6,7,8,9,10,11,13,14,15 supervivents.csv
-awk -F ',' '{if ($15 == True) cut}'
+#!/bin/bash
+cut -d ',' -f 1,2,3,4,5,6,7,8,9,10,11,13,14,15 supervivents.csv > arxiu1.csv
+X=$(wc -l < arxiu1.csv)
+awk -F ',' '{if ($14 == "False") print $0}' arxiu1.csv > arxiu2.csv
+Y=$(wc -l <arxiu2.csv)
+Z=$(($X-$Y-1))
+echo $Z
